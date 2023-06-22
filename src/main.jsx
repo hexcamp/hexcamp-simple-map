c63b47dbcc1491f4e9eae3375bc12d7e8943a350
Jim Pick <jim@jimpick.com>
2023-06-22 16:16:27 -0700

    Update libp2p

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { HeliaProvider } from '@/provider/HeliaProvider'
+// import { HeliaProvider } from '@/provider/HeliaProvider'
 
+/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HeliaProvider>
       <App />
     </HeliaProvider>
+  </React.StrictMode>
+)
+*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 )
