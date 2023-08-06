0d8f2a014db2117226b735f4ae4cb46a08845668
Jim Pick <jim@jimpick.com>
2023-08-06 15:52:06 -0700

    Add button to connect to relay

--- src/mvt/useListener.jsx
+++ src/mvt/useListener.jsx
@@ -1,7 +1,16 @@
 import { createLibp2p } from 'libp2p'
-import { webRTCStar } from '@libp2p/webrtc-star'
+
+import { circuitRelayTransport } from 'libp2p/circuit-relay'
+import { webRTC } from '@libp2p/webrtc'
+import { webSockets } from '@libp2p/websockets'
+import * as filters from '@libp2p/websockets/filters'
+
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
+
+import { identifyService } from 'libp2p/identify'
+import { pingService } from 'libp2p/ping'
+
 import { MemoryBlockstore } from 'blockstore-core'
 import { MemoryDatastore } from 'datastore-core'
 import { createHelia } from 'helia'
@@ -48,33 +57,47 @@ async function createListener (
   log,
   neighbours
 ) {
-  const star = webRTCStar()
   const node = await createLibp2p({
     peerId,
     addresses: {
-      // listen: ['/ip4/127.0.0.1/tcp/9090/wss/p2p-webrtc-star']
-      // listen: ['/ip4/192.168.1.80/tcp/9090/wss/p2p-webrtc-star']
-      listen: ['/dns4/hexcamp-webrtc-star-dev.quick.cluster-4.localnet.farm/tcp/443/wss/p2p-webrtc-star']
+      listen: ['/webrtc']
     },
-    transports: [star.transport],
+    transports: [
+      webSockets({
+        filter: filters.all
+      }),
+      webRTC(),
+      circuitRelayTransport({
+        discoverRelays: 1
+      })
+    ],
     connectionEncryption: [noise()],
     streamMuxers: [mplex()],
-    peerDiscovery: [star.discovery],
     connectionGater: {
       denyDialPeer: async incomingPeerId => {
         console.log(
           'Jim denyDialPeer',
           peerId.string,
           'Incoming',
-          incomingPeerId.string
+          incomingPeerId
         )
+        if (incomingPeerId.string === '12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN') {
+          console.log('Match: WebRTC relay')
+          return false
+        }
         const neighbourHexes = await neighbours
         console.log('Neighbours', neighbourHexes)
         const isNeighbour = neighbourHexes.has(incomingPeerId.string)
         console.log('Is neighbour?', isNeighbour)
         return !isNeighbour
       }
-    }
+    },
+    services: {
+      identify: identifyService(),
+      ping: pingService({
+        protocolPrefix: 'ipfs', // default
+      }),
+    },
   })
 
   node.addEventListener('peer:discovery', async evt => {
@@ -141,7 +164,14 @@ async function createListener (
 
   await node.start()
 
-  dispatchListenersAction({ type: 'addLibp2pNode', peerId, node })
+  console.log('Jim node', node)
+
+  console.log('listening on addresses:')
+  node.getMultiaddrs().forEach((addr) => {
+    console.log(addr.toString())
+  })
+
+  dispatchListenersAction({ type: 'addLibp2pNode', peerId, libp2pNode: node })
   log('Created libp2p node')
 
   const heliaNode = await createHeliaNode(node)
--- src/mvt/webrtc-panel.jsx
+++ src/mvt/webrtc-panel.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import useListener from './useListener'
 import { kRing } from 'h3-js'
+import { multiaddr } from '@multiformats/multiaddr'
 import getPeerIdFromH3Hex from './deterministic-peer-id'
 import hexToUrl from './hex-to-url'
 
@@ -14,15 +15,17 @@ export default function WebRTCPanel ({
 }) {
   const [neighbours, setNeighbours] = useState(new Map())
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       const hexes = kRing(hex, 1)
       const promises = []
       for (const neighbour of hexes) {
         if (neighbour === hex) continue
-        promises.push(async function() {
-          const peerId = await getPeerIdFromH3Hex(neighbour)
-          return [peerId.toString(), neighbour]
-        }())
+        promises.push(
+          (async function () {
+            const peerId = await getPeerIdFromH3Hex(neighbour)
+            return [peerId.toString(), neighbour]
+          })()
+        )
       }
       const peerIdToHex = await Promise.all(promises)
       setNeighbours(new Map(peerIdToHex))
@@ -38,6 +41,27 @@ export default function WebRTCPanel ({
     addHex
   )
 
+  function log (txt) {
+    dispatchListenersAction({ type: 'log', peerId, payload: txt })
+  }
+
+  async function connectRelay () {
+    console.log('Jim connectRelay')
+    const node = listeners[peerId.string]?.libp2pNode
+    if (!node) {
+      log('libp2p node not found')
+      return
+    }
+
+    const ma = multiaddr(
+      '/dns4/hexcamp-libp2p-webrtc-relay.quick.cluster-4.localnet.farm/tcp/443/wss/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN'
+    )
+    log(`Dialing relay '${ma}'`)
+    await node.dial(ma)
+
+    log(`Connected to relay '${ma}'`)
+  }
+
   if (!listener) {
     return (
       <div>
@@ -50,17 +74,19 @@ export default function WebRTCPanel ({
   const { peers, logs } = listener
   return (
     <div>
+      <h3>Actions</h3>
+      <button onClick={connectRelay}>Connect Relay</button>
       <h3>Peers</h3>
       <ul>
         {Object.keys(peers).map(remotePeerId => {
           const peer = peers[remotePeerId]
           return (
-            peer.connected && <li key={remotePeerId}>
-              {hexToUrl(neighbours.get(remotePeerId))}
-              &nbsp;
-              &nbsp;
-              ({remotePeerId.slice(-3)})
-            </li>
+            peer.connected && (
+              <li key={remotePeerId}>
+                {hexToUrl(neighbours.get(remotePeerId))}
+                &nbsp; &nbsp; ({remotePeerId.slice(-3)})
+              </li>
+            )
           )
         })}
       </ul>
